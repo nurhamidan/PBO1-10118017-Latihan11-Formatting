944b579df636d382255595057096ddf5468f8e45
Agung Nurhamidan <>
2019-09-22 18:12:45 +0700

    First commit.

---
+++ src/pbo1/nim10118017/Main.java
@@ -0,0 +1,38 @@
+package pbo1.nim10118017;
+
+/**
+ *
+ * @author Agung Nurhamidan
+ * NAMA                 : Agung Nurhamidan
+ * KELAS                : PBO1
+ * NIM                  : 10118017
+ * Deskripsi Program    : Melakukan formatting dari variabel yang ada kemudian menampilkannya di layar.
+ * 
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        
+        double f = 5675482.982;
+        
+        System.out.println("f : " + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
